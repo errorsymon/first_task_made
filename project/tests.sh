845287d88823d387d57c6452c9e9f7c967b5ad84
errorsymon <symon.islam@fau.de>
2024-12-07 22:10:41 +0100

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# This is the tests.sh shell script to run pytest
+
+# Ensure that you're in the right directory (optional)
+# cd /path/to/your/tests
+
+# Run pytest and point it to the tests.py file (if necessary, use the path to tests.py)
 pytest --maxfail=1 --disable-warnings -q tests.py
+
